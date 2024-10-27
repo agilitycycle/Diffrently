2e746c0daea36f273bbca7cbda0ac785c8d078ca
James Star <jamess.developer@gmail.com>
2024-10-27 22:19:27 +1300

    Update landing page

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -2,12 +2,12 @@ import { fn } from '@storybook/test';
 
 import './button.css';
 
-const baseStyles = `
-  text-white
-`;
-
-const Button = ({label, className}) => {
-  return (<button onClick={() => {}} className={className}>
+// https://storybook.js.org/docs/api/arg-types
+const Button = ({label, type, size, className}) => {
+  let additional = '';
+  if (type === 'SecondaryAction' && size === 'small') { additional = 'px-5 p-1.5' }
+  if (type === 'SecondaryAction' && size === 'medium') { additional = 'px-5 p-2.5' }
+  return (<button onClick={() => {}} className={`${className} ${additional}`}>
       <span>{label}</span>
     </button>)
 }
@@ -15,6 +15,12 @@ const Button = ({label, className}) => {
 export default {
   title: 'Example/Button',
   component: Button,
+  argTypes: {
+    size: {
+      control: 'select',
+      options: ['small', 'medium']
+    }
+  },
   parameters: {
     layout: 'centered',
   },
@@ -25,20 +31,22 @@ export default {
 export const PrimaryAction = {
   args: {
     label: 'Publish',
-    className: `${baseStyles} bg-[#f87341] rounded-full text-xl uppercase px-14 py-3.5`
+    className: 'text-white bg-[#f87341] rounded-full text-xl uppercase px-14 py-3.5'
   },
 };
 
 export const SecondaryAction = {
   args: {
+    type: 'SecondaryAction',
     label: 'Generate',
-    className: `${baseStyles} bg-blue-600 rounded-lg font-medium text-sm px-5 p-1.5`
+    size: 'small',
+    className: 'text-white bg-blue-600 rounded-lg font-medium text-sm'
   },
 };
 
 export const LoadMore = {
   args: {
     label: 'Load More',
-    className: `${baseStyles} bg-gradient-to-br from-purple-600 to-blue-500 rounded-lg font-medium text-sm px-5 py-2.5`
+    className: 'text-white bg-gradient-to-br from-purple-600 to-blue-500 rounded-lg font-medium text-sm px-5 py-2.5'
   },
 };
